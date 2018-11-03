64a8be2342f7637ba3e8c0643f436144345fe274
158Alfetta <32899555+158Alfetta@users.noreply.github.com>
2018-11-03 09:08:49 +0700

    add calender

--- script_day.js
+++ script_day.js
@@ -1,46 +1,130 @@
-// Tell to prof:
-// Instead of new Array() I'll use the faster []
+let today = new Date();
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let selectYear = document.getElementById("year");
+let selectMonth = document.getElementById("month");
 
-function myFunction(){
-    var daysOfWeek = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
+let months = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
 
-    daysOfWeek.push("Saturday");
-    daysOfWeek.unshift("Sunday");
+let monthAndYear = document.getElementById("monthAndYear");
+showCalendar(currentMonth, currentYear);
 
-    // Tell to prof:
-    // Instead of form of eval "document.write" I'll use String concatenation:
-    var table = "<table border=1><tr>";
-    for (var i=0; i < daysOfWeek.length; i++){
-        table += "<th>"+daysOfWeek[i]+"</th>";
-        // NOTE:
-        // if inside this loop we're creating the 7 <th>s headings,
-        // than we cannot assert another inner loop here
-        // to create any other part of the calendar,
-        // cause we're inside the scope of the table headings.
-        // Here we're still populating the first <tr> row!!
+
+function next() {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+}
+
+function previous() {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+}
+
+// function jump() {
+//     currentYear = parseInt(selectYear.value);
+//     currentMonth = parseInt(selectMonth.value);
+//     showCalendar(currentMonth, currentYear);
+// }
+
+function showCalendar(month, year) {
+
+    let firstDay = (new Date(year, month)).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body");
+
+    tbl.innerHTML = "";
+
+    monthAndYear.innerHTML = months[month] + " " + (year+543);
+    // selectYear.value = year;
+    // selectMonth.value = month;
+
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+
+        let row = document.createElement("tr");
+
+        for (let j = 0; j < 7; j++) {
+
+            if (i === 0 && j < firstDay) {
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+            else if (date > daysInMonth) {          
+                let cell = document.createElement("td");
+                let cellText = document.createTextNode("");
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+            }
+
+            else {
+                let cell = document.createElement("td");
+                let moonday = getMoonPhase(year, month, date);
+                let text = "";
+                if(moonday > 15){
+                    text = "วันแรม " + ((moonday%15===0)? 15: moonday%15) + " ค่ำ"
+                }else{
+                    text = "วันขึ้น " + ((moonday%15===0)? 15: moonday%15) + " ค่ำ"
+                }
+                let cellText = document.createElement("p");    
+                let h = document.createTextNode(date+"  "+text);
+                // if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
+                //     cell.classList.add("bg-info");
+                // } // color today's date
+                cellText.appendChild(h);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
+                date++;
+            }
+
+        }
+
+        tbl.appendChild(row); // appending each row into calendar body.
     }
-    table += "</tr>"; // <<< just here we're finally done with the Days headings
 
+}
 
-    // OK, now the homework: add to table rows / and 30 cells 
-    var days = 30; // How many days we need?
-    var appendEmptyDays = 7 - (days+7) % 7; // How many empty cells we miss?
+function getMoonPhase(year, month, day){
+    var c = e = jd = b = 0;
 
-    table += "<tr>";
-
-    // CREATE DAYS (and WEEKS)
-    for(var i=1; i<=days; i++){
-      table += "<td>"+ i +"</td>";
-      if(i%7===0) table += "</tr><tr>"; // Add a new week row?
+    if (month < 3) {
+        year--;
+        month += 12;
     }
 
-    // APPEND EMPTY CELLS
-    for(var i=0; i<appendEmptyDays; i++){
-      table += "<td></td>";
+    ++month;
+
+    c = 365.25 * year;
+
+    e = 30.6 * month;
+
+    jd = c + e + day - 694039.09; //jd is total days elapsed
+
+    jd /= 29.5305882; //divide by the moon cycle
+
+    b = parseInt(jd); //int(jd) -> b, take integer part of jd
+
+    jd -= b; //subtract integer part to leave fractional part of original jd
+
+    b = Math.round(jd * 30); //scale fraction from 0-8 and round
+
+    if (b >= 30) {
+        b = 0; //0 and 8 are the same so turn 8 into 0
     }
 
-    table += "</tr></table>";
-    // Done my string concatenation, I'll now use innerHTML
-    document.getElementById("calender").innerHTML = table;
+    // 0 => New Moon
+    // 1 => Waxing Crescent Moon
+    // 2 => Quarter Moon
+    // 3 => Waxing Gibbous Moon
+    // 4 => Full Moon
+    // 5 => Waning Gibbous Moon
+    // 6 => Last Quarter Moon
+    // 7 => Waning Crescent Moon
 
+    
+    return b+1;
 }
